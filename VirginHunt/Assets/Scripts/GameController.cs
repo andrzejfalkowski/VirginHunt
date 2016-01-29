fa40ca754a31b660f9ca49da9e79c45585e8f077
andrzej.falkowski <andrzej.falkowski@playsoft.fr>
2016-01-29 22:00:05 +0100

    Game Controller singleton.

--- VirginHunt/Assets/Scripts/GameController.cs
+++ VirginHunt/Assets/Scripts/GameController.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour 
+{
+	#region Singleton
+	private static GameController _instance = null;
+	
+	public static GameController Instance
+	{
+		get { return _instance; }
+	}
+	
+	private void Awake()
+	{
+		if (_instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		_instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+	#endregion
+
+	public enum EGamePhase
+	{
+		Prepare,
+		Day,
+		Night,
+		GameOver
+	}
+	public EGamePhase CurrentGamePhase = EGamePhase.Prepare;
+	private float CurrentPhaseTime = 0f;
+
+	public bool IsGameInPogress()
+	{
+		return CurrentGamePhase == EGamePhase.Day || CurrentGamePhase == EGamePhase.Night;
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
