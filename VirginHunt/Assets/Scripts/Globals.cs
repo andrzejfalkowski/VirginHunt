7aaeedb38750ffa3a64f511803052837a3c12a4d
Mateusz Serdynski <mateusz.serdynski@gmail.com>
2016-01-29 23:54:50 +0100

    Update Global - Villager movement speed

--- VirginHunt/Assets/Scripts/Globals.cs
+++ VirginHunt/Assets/Scripts/Globals.cs
@@ -12,6 +12,8 @@ public static class Globals
 	public const float VILLAGERS_MAX_X = 50f;
 	public const float VILLAGERS_MIN_X = -50f;
 
+    public const float VILLAGER_MOVEMENT_SPEED = 1f;
+
 	public const int START_BEAST_AMOUNT = 2;
 	public const int BEAST_AMOUNT_INCREASE = 1;
 	public const float BEAST_MERCY_TIME = 2f;
