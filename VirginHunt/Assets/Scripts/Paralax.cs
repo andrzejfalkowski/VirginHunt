e352366fab37afed3f5d30dc922461aefe476165
andrzej.falkowski <andrzej.falkowski@playsoft.fr>
2016-01-29 23:07:43 +0100

    Paralax script.

---
+++ VirginHunt/Assets/Scripts/Paralax.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralax : MonoBehaviour 
+{
+	private float xModifier = 0f;
+	private float yModifier = 0f;
+
+	private float xStartShift = 0f;
+	private float yStartShift = 0f;
+
+	void Start() 
+	{
+		xStartShift = this.transform.localPosition.x;
+		yStartShift = this.transform.localPosition.y;
+	}
+
+	void Update() 
+	{
+		Vector3 pos = this.transform.localPosition;
+		pos.x = xStartShift + GameController.Instance.MainCamera.transform.localPosition.x * xModifier;
+		pos.y = yStartShift + GameController.Instance.MainCamera.transform.localPosition.y * yModifier;
+	}
+}
