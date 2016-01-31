595b27b778a4a7d0f2c70f3db30391ca26a3e80a
Mateusz Serdynski <mateusz.serdynski@gmail.com>
2016-01-31 14:56:50 +0100

    AAA

---
+++ VirginHunt/Assets/Scripts/WinGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinGame : MonoBehaviour {
+
+    public GameObject WinGamePanel;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Globals.POWER >= Globals.MAX_POWER)
+        {
+            WinGamePanel.SetActive(true);
+        }
+	}
+}
