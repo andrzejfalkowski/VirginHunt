c1b82c746b882128ae22b3ef2ff81298b3e7afd5
andrzej.falkowski <andrzej.falkowski@playsoft.fr>
2016-01-31 09:55:32 +0100

    Aura and flying dots.

---
+++ VirginHunt/Assets/StartAnimationWithDelay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartAnimationWithDelay : MonoBehaviour 
+{
+	public Animator DelayedAnimator;
+	public float Delay = 0f;
+	float timer = 0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if(timer > Delay)
+		{
+			DelayedAnimator.enabled = true;
+			Destroy(this);
+		}
+	}
+}
