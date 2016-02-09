174f5454818ed31c1688bca2726b8648d02642ba
andrzej.falkowski <andrzej.falkowski@playsoft.fr>
2016-02-09 19:31:46 +0100

    Environment gets lighter and darker depending on time of the day.

---
+++ VirginHunt/Assets/MoodController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoodController : MonoBehaviour 
+{
+	public float Modifier = 1f;
+	SpriteRenderer[] sprites;
+
+	void Start () 
+	{
+		sprites = GetComponentsInChildren<SpriteRenderer>();
+	}
+
+	void Update()
+	{
+		float power = 0f;
+		if(GameController.Instance.CurrentGamePhase == GameController.EGamePhase.Day)
+		{
+			power = 1f - Mathf.Abs((Globals.DAY_DURATION / 2f) - GameController.Instance.CurrentPhaseTime) / (Globals.DAY_DURATION / 2f);
+		}
+		else
+			power = 0f;
+
+		power = power * Modifier + (1 - Modifier);
+		//Debug.Log(power);
+		foreach(var sprite in sprites)
+		{
+			sprite.color = new Color(power + 0.05f, power, power + 0.1f, 1f);
+		}
+		
+	}
+}
